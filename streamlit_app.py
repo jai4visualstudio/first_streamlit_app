ad630d99a63836894b8fa22804232ebfa3a16c56
Jainendra Kumar <108861811+jai4visualstudio@users.noreply.github.com>
2022-07-07 13:29:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket 
