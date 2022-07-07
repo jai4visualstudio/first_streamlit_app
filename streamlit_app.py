4b80103d50aa4f3c21a256f6fe5cc579fd826394
Jainendra Kumar <108861811+jai4visualstudio@users.noreply.github.com>
2022-07-07 13:45:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🍛Kale, Spinach & Rocket smoothie')
 streamlit.text('🍤Hard boiled free range Egg')
 
 streamlit.header('🍓🍓🍅Build your own fruit smoothie🍂🍂')
+
+
+import pandas
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
