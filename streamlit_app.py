2a24c384cb96842cfd2a161e7442e35f30fd7702
Jainendra Kumar <108861811+jai4visualstudio@users.noreply.github.com>
2022-07-07 13:41:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Parents new healthy dinner')
+streamlit.title('Break Smoothie')
 streamlit.header('🎉Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍛Kale, Spinach & Rocket smoothie')
 streamlit.text('🍤Hard boiled free range Egg')
 
+streamlit.header('🍓🍓🍅Build your own fruit smoothie🍂🍂')
