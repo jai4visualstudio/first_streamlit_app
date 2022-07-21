dd44aa75eedfd7dc9f17800758d012a0ad415eab
Jainendra Kumar <108861811+jai4visualstudio@users.noreply.github.com>
2022-07-21 14:23:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-
-  from urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('Break Smoothie')
 streamlit.header('🎉Breakfast Menu')
