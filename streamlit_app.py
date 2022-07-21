d457b659cad14c1cc803668b6b10e20a79e7b20a
Jainendra Kumar <108861811+jai4visualstudio@users.noreply.github.com>
2022-07-21 15:45:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,6 @@ if steamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
   streamlit.dataframe(my_data_row)
-)
+
 
 
